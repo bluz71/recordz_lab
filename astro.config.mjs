749281031b9f094ff58a059d9e9090e3d380f5e7
Dennis B <bluz71@users.noreply.github.com>
2023-12-26 15:04:48 +1100

    Integrate HTMX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
 
 import alpinejs from '@astrojs/alpinejs'
+import htmx from 'astro-htmx'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [alpinejs()]
+  integrations: [alpinejs(), htmx()]
 })
